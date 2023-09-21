f500c65d3773c930db86fa855748f432c55f66a4
Yana <yahramenkova@mail.ru>
2023-09-21 17:54:40 +0300

    feat: add function to switch topics

--- function.js
+++ function.js
@@ -28,3 +28,16 @@ fetch('translate.json')
     getTranslate('en');
   })
   .catch(error => console.error('Ошибка при загрузке json файла:', error));
+
+  const themeToggleBtn = document.getElementById('theme-toggle');
+  const elementsToToggle = document.querySelectorAll('.content');
+  let isDarkTheme = false;
+
+  themeToggleBtn.addEventListener('click', () => {
+    isDarkTheme = !isDarkTheme;
+    if (isDarkTheme) {
+        document.body.classList.add('dark-theme');
+    } else {
+        document.body.classList.remove('dark-theme');
+    }
+});
