9dff120f3a907db68c40813609b09f19d0dc2c3c
Yana <yahramenkova@mail.ru>
2023-09-21 17:33:12 +0300

    feat: add function for translate

---
+++ function.js
@@ -0,0 +1,30 @@
+fetch('translate.json')
+  .then(response => response.json())
+  .then(i18Obj => {
+    // Функция для перевода страницы на указанный язык
+    function getTranslate(language) {
+      const elementsWithTranslation = document.querySelectorAll('[data-i18n]');
+
+      elementsWithTranslation.forEach((element) => {
+        const key = element.dataset.i18n;
+        const translation = i18Obj[language][key];
+
+        if (translation) {
+          element.textContent = translation;
+        }
+      });
+    }
+
+    const switchToEnglish = document.getElementById('English');
+    const switchToRussian = document.getElementById('Russian');
+
+    switchToEnglish.addEventListener('click', () => {
+      getTranslate('en');
+    });
+
+    switchToRussian.addEventListener('click', () => {
+      getTranslate('ru');
+    });
+    getTranslate('en');
+  })
+  .catch(error => console.error('Ошибка при загрузке json файла:', error));
